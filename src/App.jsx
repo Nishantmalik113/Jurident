243c350b6d46ef447edcc90ec08cef7f9203b292
Nishantmalik113 <nishantmalik113@gmail.com>
2025-02-24 00:20:54 +0530

    snap

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import Hero from './components/Hero'
 import MyFile from './components/MyFile'
 import Navbar from './components/Navbar'
+import Register from './components/Register'
 
 function App() {
 
   return (
     <>
-      <Hero />
-      <MyFile />
+    <Hero/>
+    <MyFile/>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,43 +21,43 @@ window.scrollTo(0,0)
         <img src="../ellipse-top.png" className='absolute rotate-27 right-[-25vw] top-[-20vh] sm:right-[-150px] sm:top-[-200px] z-102' alt="" />
         <img src="../ellipse-bottom.png" className='absolute sm:bottom-[-300px] bottom-[-20vh]' alt="" />
         <img src="../JURIDENT.png" className='absolute left-5' alt="" />
-        <div ref={refContainer} className='carousel relative items-center mx-20'>
-            <div className='Carousel_card flex flex-col items-center' >
-                <img src="../Jurident-Logo.png" className=' z-101' alt="" />
+        <div ref={refContainer} className='carousel snap-x snap-mandatory relative items-center mx-20'>
+            <div className='Carousel_card snap-center flex flex-col items-center' >
+                <img src="../Jurident-Logo.png" className='sm:w-fit w-[350px] z-101' alt="" />
             </div>
-            <div className='Carousel_card flex flex-col items-center gap-10' >
-                <div className='flex gap-10 items-center justify-center w-[450px]'>
+            <div className='Carousel_card  snap-center flex flex-col items-center gap-10' >
+                <div className='flex gap-10 items-center justify-center w-[150px] sm:w-[450px]'>
                     <img src="../organised.png" className=' z-101' alt="" />
                     <div className='flex flex-col gap-5'>
-                        <h1 className='text-5xl font-semibold text-[#C99F4A]'>Stay Organised</h1>
-                        <p className='text-3xl text-white'>Ensure that your schedule is accurate by entering new appointments or events into our digital calender.</p>
+                        <h1 className=' text-3xl sm:text-5xl font-semibold text-[#C99F4A]'>Stay Organised</h1>
+                        <p className='sm:text-3xl text-white'>Ensure that your schedule is accurate by entering new appointments or events into our digital calender.</p>
                     </div>
                 </div>
                 <img src="../organised-nav.png" alt="" />
             </div>
-            <div className='Carousel_card flex flex-col items-center gap-10' >
-            <div className='flex gap-10 items-center justify-center w-[650px]'>
-                    <img src="../late.png" className=' z-101' alt="" />
+            <div className='Carousel_card snap-center flex flex-col items-center gap-10' >
+            <div className='flex items-center justify-center w-[350px] sm:w-[650px]'>
+                    <img src="../late.png" className='w-[200px] sm:w-full z-101' alt="" />
                     <div className='flex flex-col gap-5'>
-                        <h1 className='text-5xl font-semibold text-[#C99F4A]'>Never be Late</h1>
-                        <p className='text-3xl text-white'>Our alarm system integrated within the app will notify you of any upcoming events.
+                        <h1 className='text-3xl sm:text-5xl font-semibold text-[#C99F4A]'>Never be Late</h1>
+                        <p className='sm:text-3xl text-white'>Our alarm system integrated within the app will notify you of any upcoming events.
                         </p>
                     </div>
                 </div>
                 <img src="../late-nav.png" alt="" />
             </div>
-            <div className='Carousel_card flex flex-col items-center gap-10' >
-            <div className='flex gap-10 items-center justify-center w-[450px]'>
+            <div className='Carousel_card snap-center flex flex-col items-center gap-10' >
+            <div className='flex gap-10 items-center justify-center w-[150px] sm:w-[450px]'>
                     <img src="../everything.png" className=' z-101' alt="" />
                     <div className='flex flex-col gap-5'>
-                        <h1 className='text-5xl font-semibold text-[#C99F4A]'>Everything in one Place</h1>
-                        <p className='text-3xl text-white'>Add your personal notes to any case you'd prefer.</p>
+                        <h1 className='text-3xl sm:text-5xl font-semibold text-[#C99F4A]'>Everything in one Place</h1>
+                        <p className='sm:text-3xl text-white'>Add your personal notes to any case you'd prefer.</p>
                     </div>
                 </div>
                 <img src="../everything-nav.png" alt="" />
             </div>
         </div>
-        <div className='absolute z-103 flex items-center justify-center sm:gap-[80vw] gap-[50vw]'>
+        <div className='absolute opacity-0 sm:opacity-[1] pointer-events-none sm:pointer-events-auto z-103 flex items-center justify-center sm:gap-[80vw] gap-[50vw]'>
             <button className=' w-15 sm:w-20' onClick={()=>{
                       ScrollContainer.style.scrollBehavior = "smooth"
                       ScrollContainer.scrollLeft -= width
