b5bd3f195fb66b5f83e9e4600f2003047f8a8d6c
Nishantmalik113 <nishantmalik113@gmail.com>
2025-02-24 00:22:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import React from 'react'
 import Hero from './components/Hero'
 import MyFile from './components/MyFile'
-import Navbar from './components/Navbar'
-import Register from './components/Register'
 
 function App() {
 
